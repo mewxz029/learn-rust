1cb3ecb5882c38ba2a0fdc95a8cc59e2fe670c41
mewxz029 <mewlawbreakertv@gmail.com>
2024-09-21 14:09:38 +0700

    add convert temperatures in chapter 3 practice

--- projects/chapter_3/practice/src/main.rs
+++ projects/chapter_3/practice/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     println!("nth: {}", fibonacci(10));
     println!("nth: {}", fibonacci_memoized(10, 0, 1));
+
+    convert_temperature(32.0, 'f');
+    convert_temperature(32.0, 'c');
 }
 
 fn fibonacci(n: u32) -> u32 {
@@ -16,4 +19,18 @@ fn fibonacci_memoized(n: u32, a: u32, b: u32) -> u32 {
         return a;
     }
     fibonacci_memoized(n - 1, b, a + b)
+}
+
+fn convert_temperature(x: f64, target_unit: char) {
+    if target_unit == 'c' {
+        println!(
+            "fahrenheit to degree celsius: {:.2}",
+            ((x - 32.0) * 5.0 / 9.0)
+        );
+    } else {
+        println!(
+            "degree celsius to fahrenheit: {:.2}",
+            ((x * 9.0 / 5.0) + 32.0)
+        );
+    }
 }
